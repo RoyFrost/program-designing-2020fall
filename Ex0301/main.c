11d969327594dee84b138b05a56709a2cb8b133f
RoyFrost <972469981@qq.com>
2020-10-22 11:11:42 +0800

    progressing on Ex0301

---
+++ Ex0301/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "..\zylib\zylib.c"
+
+int IsPrime(int num)
+{
+    unsigned int i = 3;
+    if (num < 2) return 0;
+    else if ( num == 2 ) return 1;
+    else
+    {
+        while (i <= (int)sqrt(num)+1/)
+        {
+            if (num % i==0) return 0;
+            i+=2;
+        }
+    }
+    return 1;
+}
+
+
+int main()
+{
+    int temp;
+    temp = GetIntegerFromKeyboard();
+    if (IsPrime(temp)) printf("true!");
+    else printf("false!");
+    return 0;
+}
